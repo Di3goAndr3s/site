2f34b37d5f32e579460f5cfbbd73dd8c1d459c4a
diegovelasquez <diego.velasquez@imaginamos.co>
2014-08-05 11:47:49 -0500

    primer commit

---
+++ production/header.php
@@ -0,0 +1,5 @@
+
+<header>
+  <div class="wrapper"><a class="branding"><span>+</span><span>DV</span>
+      <p>Front-end Developer</p></a></div>
+</header>
---
+++ production/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<hmtl lang="es">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <title>Diego Velásquez - FRONTEND DEVELOPER SENIOR - Inicio</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+    <meta http-equiv="content-language" content="es"/>
+    <meta http-equiv="pragma" content="No-Cache"/>
+    <meta name="Description" lang="es" content=""/>
+    <meta name="copyright" content="diegovelasquez.co"/>
+    <meta name="date" content="2014"/>
+    <meta name="author" content="diseño web: diegovelasquez.co"/>
+    <meta name="robots" content="All"/>
+    <link rel="author" type="text/plain" href="humans.txt"/>
+    <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
+    <script src="assets/js/lib/jquery-1.11.0.min.js"></script>
+    <script src="assets/js/main.js"></script>
+  </head>
+  <body><?php include("header.php"); ?></body>
+</hmtl>
